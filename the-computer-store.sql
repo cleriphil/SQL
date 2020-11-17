58b97555043d4a66aac9991908ca84df3998feb3
cleriphil <cleri.phil@gmail.com>
2020-11-17 00:00:47 -0800

    add exercises 6-10

--- the-computer-store.sql
+++ the-computer-store.sql
@@ -23,3 +23,30 @@ SELECT * FROM products WHERE price BETWEEN 60 AND 120;
 SELECT Name, price * 100 FROM products;
 -- OR
 SELECT Name, price * 100 AS priceCents FROM products;
+
+-- 6. Compute the average price of all the products.~
+
+SELECT AVG(Price) FROM products;
+
+-- 7. Compute the average price of all products with manufacturer code equal to 2.
+
+SELECT AVG(price) FROM products WHERE manufacturer = 2;
+
+-- 8. Compute the number of products with a price larger than or equal to $180.
+
+SELECT COUNT(*) FROM products WHERE price >= 180;
+
+-- 9. Select the name and price of all products with a price larger than or equal to $180, and sort first by price (in descending order), and then by name (in ascending order).~
+
+SELECT name, price FROM products WHERE price >=180  ORDER BY price DESC, name;
+
+-- 10. Select all the data from the products, including all the data for each product's manufacturer.~
+
+--  Without INNER JOIN 
+SELECT * FROM products, manufacturers
+WHERE products.manufacturer = manufacturers.code;
+
+-- With INNER JOIN 
+SELECT *
+FROM products INNER JOIN manufacturers
+ON products.manufacturer = manufacturers.code;
