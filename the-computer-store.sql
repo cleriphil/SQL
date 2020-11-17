aefb4c0659b30738ca5c60a41f55247e7ad38cbd
cleriphil <cleri.phil@gmail.com>
2020-11-16 23:38:31 -0800

    add exercise 1-5

---
+++ the-computer-store.sql
@@ -0,0 +1,14 @@
+-- 1. Select the names of all the products in the store.
+
+
+-- 2. Select the names and the prices of all the products in the store.
+
+
+-- 3. Select the name of the products with a price less than or equal to $200.
+
+
+-- 4. Select all the products with a price between $60 and $120.
+
+
+-- 5. Select the name and price in cents (i.e., the price must be multiplied by 100).
+
